220cec2b4623e03a3be4d34c35d1051ab863aa1f
ndaga1603 <davidndaga1603@gmail.com>
2022-04-14 09:36:28 -0700

    now the game become more useful for learning purpose

--- main.py
+++ main.py
@@ -17,12 +17,18 @@ result.hideturtle()
 states = pandas.read_csv("50_states.CSV")
 
 all_states = []
+
+# checks if all states are correctly entered
 while len(all_states) < 50:
     user_input = screen.textinput(
-        title=f"{len(all_states)}/50 U.s States:", prompt="Enter any state you know")
+        title=f"{len(all_states)}/50 U.s States:", prompt="Enter any state you know").title()
 
+    if user_input == "Exit":
+        break
     for st in states.state:
-        if st.lower() == user_input.lower():
+
+        # checks if user input matches any of the state in US
+        if st == user_input:
             all_states.append(user_input)
             x_cor = int(states[states.state == st].x)
             y_cor = int(states[states.state == st].y)
@@ -31,15 +37,16 @@ while len(all_states) < 50:
             result.write(f"{user_input}")
             
 
+# creates a file to show all missed states
 
-turtle.mainloop()
+st_list = states.state.to_list()
 
+missed_states = []
+for element in st_list:
+    if element not in all_states:
+        missed_states.append(element)
 
-# function to get coroutines from the screen
-# def get_coor(x, y):
-#     with open("coor.txt", "w") as f:
-#         x_cor = str(x)
-#         y_cor = str(y)
-#         f.write(x_cor)
-#         f.write(y_cor)
-# turtle.onscreenclick(fun=get_coor)
+pd = pandas.Series(missed_states)
+pd.to_csv("Missed States.CSV")
+   
+
